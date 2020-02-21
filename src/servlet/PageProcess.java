10d2ba233c39653f26d25dfe341d0b02765cee3e
Aereli <aerelioyanadel@gmail.com>
2020-02-21 11:54:49 -0500

    jdbc connector files

--- src/servlet/PageProcess.java
+++ src/servlet/PageProcess.java
@@ -1,11 +1,15 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.DriverManager;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.mysql.jdbc.Connection;
 
 /**
  * Servlet implementation class PageProcess
@@ -28,6 +32,7 @@ public class PageProcess extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
 	}
 
 	/**
